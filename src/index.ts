6120d79288ac3fc2ae53820c5c6b974aff15adda
harrydehix <harry.holly02@gmail.com>
2024-07-22 10:43:19 +0200

    Update owvision environment

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,25 @@
-import { CommandResponse, Record, WeatherStationInterface, setup } from "owvision-environment/interfaces";
-import { BaudRate, RainCollectorSize } from "vant-environment/structures";
+import { CommandResponse, Record, WeatherStationInterface, setup, Argument } from "owvision-environment/interfaces";
+import { BaudRate, BaudRates, RainCollectorSize, RainCollectorSizes } from "vant-environment/structures";
 import { WeatherStationAdvanced } from "vantjs/weather-station";
 import { AdvancedRealtimeDataContainer } from "vantjs/realtime-containers";
 import { waitForNewSerialConnection } from "vantjs/util";
 
 export type Config = {
-    // serial_path: string,
-    baud_rate: BaudRate,
-    rain_collector_size: RainCollectorSize,
+    serial_path: Argument<string>,
+    baud_rate: Argument<BaudRate>,
+    rain_collector_size: Argument<RainCollectorSize>,
 }
 
 class AdvancedDavisInterface extends WeatherStationInterface<Config>{
     private station?: WeatherStationAdvanced;
     private realtime_container?: AdvancedRealtimeDataContainer;
-    config: Config = {
-        baud_rate: 19200,
-        rain_collector_size: "0.1mm"
-    }
 
     async connect() {
         // const serial_path = await waitForNewSerialConnection();
         this.station = await WeatherStationAdvanced.connect({
-            path: "/dev/ttyUSB0",
-            rainCollectorSize: this.config.rain_collector_size,
-            baudRate: this.config.baud_rate,
+            path: this.config.serial_path.value,
+            rainCollectorSize: this.config.rain_collector_size.value,
+            baudRate: this.config.baud_rate.value,
         });
         this.realtime_container = this.station.createDetailedRealtimeDataContainer({
             updateInterval: 1,
